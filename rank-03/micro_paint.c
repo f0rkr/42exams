a997977e34ee0ccdaa6a2bb08cc729584e45c681
Mohamed Ashad <mashad@e1r10p10.1337.ma>
2020-12-25 12:36:23 +0100

    rank-03

--- rank-03/micro_paint.c
+++ rank-03/micro_paint.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <math.h>
 
 typedef struct		s_display
 {
@@ -45,21 +46,6 @@ int		ft_check_validity(float x, float y, float xb, float yb)
 	return (0);
 }
 
-int		ft_check_empty(int x, int y, int xb, int yb)
-{
-	if ((x == g_r.xt || x == xb) || (y == g_r.yt || y == yb))
-		return (1);
-	return (0);
-}
-
-void	ft_draw(float xt, float yt, float xb, float yb)
-{
-	if (x >= 0 && x < g_display.width)
-		if (y >= 0 && y < g_display.height)
-			return (1);
-	return (0);
-}
-
 int		ft_check_empty(float x, float y, float xb, float yb)
 {
 	if ((x == g_r.xt || x == xb) || (y == g_r.yt || y == yb))
@@ -79,15 +65,15 @@ void	ft_draw(float xt, float yt, float xb, float yb)
 		xt = g_r.x;
 		while (xt <= xb)
 		{
-			if (g_r.op == 'r' && ft_check_validity(xt, yt, xb, yb) && ft_check_empty(xt, yt, xb, yb))
+			if (g_r.op == 'r' && ft_check_validity(round(xt), round(yt), xb, yb) && ft_check_empty(round(xt), round(yt), round(xb), round(yb)))
 				g_display.mapzone[(int)yt][(int)xt] = g_r.b_char;
-			else if (g_r.op == 'R' && ft_check_validity(xt, yt, xb, yb))
+			else if (g_r.op == 'R' && ft_check_validity(round(xt), round(yt), xb, yb))
 				g_display.mapzone[(int)yt][(int)xt] = g_r.b_char;
-			// else
-			// {
-			// 	if (ft_check_validity(xt, yt, xb, yb))
-			// 		printf("NO: %c:%c %f:%f %f:%f %f %f check_validity: %d check_empty: %d\n", g_r.op, g_r.b_char, g_r.xt, xt, g_r.yt, yt, g_r.xb, g_r.yb, ft_check_validity(xt, yt, xb, yb), ft_check_empty(xt, yt, xb, yb));
-			// }
+			else
+			{
+				if (ft_check_validity(xt, yt, xb, yb))
+					printf("NO: %c:%c %f:%f %f:%f %f:%f %f:%f check_validity: %d check_empty: %d\n", g_r.op, g_r.b_char, g_r.xt, round(xt), g_r.yt, round(yt), g_r.xb, xb, g_r.yb, yb, ft_check_validity(xt, yt, xb, yb), ft_check_empty(round(xt), round(yt), xb, yb));
+			}
 			
 			xt++;
 		}
@@ -114,22 +100,18 @@ int     ft_readfile(char const *filename)
 	}
 	while ((i = fscanf(fd, "%c %f %f %f %f %c \n", &g_r.op, &g_r.x, &g_r.y, &g_r.width, &g_r.height, &g_r.b_char)) == 6)
 	{
-		g_r.x += 0.05;
-		g_r.y += 0.05;
 		g_r.xb = g_r.x + g_r.width;
 		g_r.yb = g_r.y + g_r.height;
-		if (((int) (g_r.x * 10) % 10) > 0)
-			g_r.x++;
-		if (((int) (g_r.y * 10) % 10 ) > 0)
-			g_r.y++;
-		if (((int) (g_r.width * 10) % 10) > 7)
-			g_r.xb++;
-		if (((int) (g_r.height * 10) % 10) > 7)
-			g_r.yb++;
-		g_r.xt = (int) g_r.x;
-		g_r.yt = (int) g_r.y;
-		// printf("%c %f %f %f %f\n", g_r.op, g_r.x, g_r.y, g_r.width, g_r.height);
-		// printf("%c %f %f %f %f\n", g_r.op, g_r.xt, g_r.yt, g_r.xb, g_r.yb);
+		g_r.xt = round(g_r.x);
+		g_r.yt = round(g_r.y);
+		// if ((int) (g_r.x * 10) % 10 > 0 || (int) (g_r.y * 10) % 10 >= 0)
+		// {
+		// 	g_r.xb = round(g_r.xb);
+		// 	g_r.yb = round(g_r.yb);
+		// }
+		printf("%f : floor: %f\n", g_r.x, round(g_r.x));
+		printf("%c %f %f %f %f\n", g_r.op, g_r.x, g_r.y, g_r.width, g_r.height);
+		printf("%c %f %f %f %f\n", g_r.op, g_r.xt, g_r.cat yt, g_r.xb, g_r.yb);
 		ft_draw(g_r.xt, g_r.yt, g_r.xb , g_r.yb);
 	}
 	fclose(fd);
